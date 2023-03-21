d99af6201943872666f1f027a121bf1fa93da39e
Allen Febi M A <105870053+all3n2601@users.noreply.github.com>
2023-03-21 18:39:26 +0530

    Update app.py

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import pickle
 import streamlit as st
 from streamlit_option_menu import option_menu
 
-heart_disease_model = pickle.load(open('/Users/allenabi/Desktop/College/MachineLearningTheory/Project/heart_disease_model.sav','rb'))
+heart_disease_model = pickle.load(open('/heart_disease_model.sav','rb'))
 
 
 
