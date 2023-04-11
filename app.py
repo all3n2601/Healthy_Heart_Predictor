975cfa746bddf6d30a70c3b9f651c8d40ac2a784
Allen Febi M A <105870053+all3n2601@users.noreply.github.com>
2023-04-11 14:09:54 +0530

    Update app.py

--- app.py
+++ app.py
@@ -78,7 +78,7 @@ if (selected == 'Healthy Heart Prediction'):
         heart_prediction = heart_disease_model.predict([[age, sex, cp, trestbps, chol, fbs, restecg,thalach,exang,oldpeak,slope,ca,thal]])                         
         
         if (heart_prediction[0] == 1):
-          heart_diagnosis = "The person is not Healthy 🫀 and Has a Heart Disease🙁
+          heart_diagnosis = "The person is not Healthy 🫀 and Has a Heart Disease"
         -----------------------------------------------------------------------------------------------------
                          Sunday    | Monday  |  Tuesday  |  Wednesday  |  Thursday  |  Friday  |  Saturday  |
         
